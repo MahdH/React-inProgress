49683cf9131e9cba527c1361468c3d2e97ff77a0
MythMahdi <insta.carpoll20@gmail.com>
2020-03-26 12:40:51 +0430

    ch1

--- src/component/contact/AddContact.jsx
+++ src/component/contact/AddContact.jsx
@@ -9,9 +9,12 @@ class AddContact extends Component {
         phone: ''
     };
 
-
+    onChange = (e) => this.setState( {[e.target.name] : e.target.value} );
 
     render() {
+
+        const {name, email, phone } = this.state
+
         return (
             <div className="card mb-3">
                 <div className="card-header">
@@ -20,11 +23,40 @@ class AddContact extends Component {
                             <label htmlFor="name">Name</label>
                             <input 
                             type="text"
+                            name="name"
                             placeholder="Enter name..."
+                            className="form-control form-control-sm"
                             value={name}
-                                
+                            onChange={this.onChange}
                             />
                         </div>
+                        <div className="form-group">
+                            <label htmlFor="email">email</label>
+                            <input 
+                            type="text"
+                            name="email"
+                            placeholder="Enter email..."
+                            className="form-control form-control-sm"
+                            value={email}
+                            onChange={this.onChange}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="phone">phone</label>
+                            <input 
+                            type="text"
+                            name="phnoe"
+                            placeholder="Enter phone..."
+                            className="form-control form-control-sm"
+                            value={phone}
+                            onChange={this.onChange}
+                            />
+                        </div>
+                        <input 
+                        type="submit"
+                        value="Add Contact"
+                        className="btn btn-block"
+                        />
                     </form>
                 </div>
             </div>
