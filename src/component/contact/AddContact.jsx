d61c8497f1c54d0c6a0f84f3125ed7ad60d26cf8
MythMahdi <insta.carpoll20@gmail.com>
2020-03-26 14:32:53 +0430

    code format

--- src/component/contact/AddContact.jsx
+++ src/component/contact/AddContact.jsx
@@ -9,57 +9,57 @@ class AddContact extends Component {
         phone: ''
     };
 
-    onChange = (e) => this.setState( {[e.target.name] : e.target.value} );
+    onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
     render() {
 
-        const {name, email, phone } = this.state
+        const { name, email, phone } = this.state
 
         return (
             <div className="card mb-3">
                 <div className="card-header">Add Contact</div>
                 <div className="card-body">
-                        <form>
-                            <div className="form-group">
-                                <label htmlFor="name">Name</label>
-                                <input 
+                    <form>
+                        <div className="form-group">
+                            <label htmlFor="name">Name</label>
+                            <input
                                 type="text"
                                 name="name"
                                 placeholder="Enter name..."
                                 className="form-control form-control-lg"
                                 value={name}
                                 onChange={this.onChange}
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="email">Email</label>
-                                <input 
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input
                                 type="email"
                                 name="email"
                                 placeholder="Enter email..."
                                 className="form-control form-control-lg"
                                 value={email}
                                 onChange={this.onChange}
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="phone">Phone</label>
-                                <input 
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="phone">Phone</label>
+                            <input
                                 type="text"
                                 name="phone"
                                 placeholder="Enter phone..."
                                 className="form-control form-control-lg"
                                 value={phone}
                                 onChange={this.onChange}
-                                />
-                            </div>
-                            <input 
+                            />
+                        </div>
+                        <input
                             type="submit"
                             value="Add Contact"
                             className="btn btn-dark btn-block"
-                            />
-                        </form>
-                    </div>
+                        />
+                    </form>
+                </div>
             </div>
         )
     }
